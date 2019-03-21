6091ff8418c1879c94b5f091f3e3205e97a1d337
GitHubforMatt <xuzhedemail@163.com>
2019-03-21 19:23:32 +0800

    Delete add_start_url.py

--- example_scrapy/add_start_url.py
+++
@@ -1,7 +0,0 @@
-# -*- coding:utf-8 -*-
-import redis
-
-myredis = redis.Redis(host="192.168.0.86", password="root123456", port=6379)
-print(myredis.info())
-url = "https://www.dy2018.com/html/gndy/dyzz/index.html"
-myredis.lpush("film_spider:start_urls", url)
